c26b1f5c5a200c5a464007624940e1c7210ce705
Eoin McMahon <eoin.mcmahon1@ucdconnect.ie>
2020-10-05 09:32:11 +0100

    Lab work onetwo

---
+++ onetwo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("o");
+  printf("n");
+  printf("e");
+  printf("\n");
+  printf("t");
+  printf("w");
+  printf("o");
+  printf("\n");
+
+  return 0;
+}
