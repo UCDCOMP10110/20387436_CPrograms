c0c890bf0b7d5c258347e51e0cbe0eff8ae6bece
Eoin McMahon <eoin.mcmahon1@ucdconnect.ie>
2020-10-08 13:08:25 +0100

    Pacman final form

--- pacman.c
+++ pacman.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+# include <stdio.h>
+void Go( int steps ) ;
 
-void Go(int steps);
-
-int main(void)
+int main ( void )
 {
-  int sx , sy , dx , dy ;
-  int diff_x , diff_y;
+  int sx , sy , dx , dy;
+  int diff_x , diff_y ;
 
   sx = 2;
   sy = 5;
@@ -15,46 +14,46 @@ int main(void)
   diff_x = dx - sx;
   diff_y = dy - sy;
 
-  if (diff_x> 0 && diff_y > 0)
+  if ( diff_x >0 && diff_y > 0)
     {
-      Go(1);
       printf("R ");
-      Go(diff_x);
       printf("R ");
-      Go(diff_y);
+      Go( diff_y ) ;
+      printf ("L ") ;
+      Go( diff_x ) ;
     }
-    if (diff_x>0 && diff_y < 0)
+
+  if ( diff_x >0 && diff_y < 0)
     {
-      Go(1);
-      printf("L ");
-      Go(-diff_x);
-      printf("L ");
-      Go(diff_y);
+      Go( - diff_y ) ;
+      printf ("R ") ;
+      Go( diff_x ) ;
     }
-    if (diff_x<0 && diff_y > 0)
+
+  if ( diff_x <0 && diff_y > 0)
     {
-      Go(1);
-      printf("L ");
-      Go(-diff_x);
-      printf("L ");
-      Go(diff_y);
-    }
-    if (diff_x<0 && diff_y < 0)
-    {
-      Go(1);
-      printf("L ");
-      Go(-diff_x);
       printf("R ");
-      Go(-diff_y);
+      printf("R ");
+      Go( diff_y ) ;
+      printf ("R ") ;
+      Go( - diff_x ) ;
     }
-  printf("\n");
+
+  if ( diff_x <0 && diff_y < 0)
+    {
+      Go( - diff_y ) ;
+      printf("L ");
+      Go( - diff_x ) ;
+    }
+
+  printf ("\n") ;
   return 0;
 }
-void Go(int steps)
+void Go( int steps )
 {
   int i;
-  for (i=0 ; i<steps;i++)
+  for (i=0;i< steps ;i++)
     {
-      printf("W ");
+      printf ("W ") ;
     }
 }
