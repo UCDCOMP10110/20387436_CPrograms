a5d8192f630ba5bd732817ccb5b2c6c00555d7a6
Eoin McMahon <eoin.mcmahon1@ucdconnect.ie>
2020-10-10 14:47:40 +0100

    char variables more complicated

--- char.c
+++ char.c
@@ -2,9 +2,23 @@
 
 int main(void)
 {
-  char ch = 'a';
-  ch = ch + 1;
-  printf("%c\n" , ch);
+  char t = 'r';
+  char h = 'u';
+  char i = 'c';
+  char s = 'c';
+  char is = 'e';
+  char a = 's';
+  char test = 's';
+
+  t = t + 1;
+
+  printf("%c" , t);
+  printf("%c" , h);
+  printf("%c" , i);
+  printf("%c" , s);
+  printf("%c" , is);
+  printf("%c" , a);
+  printf("%c" , test);
 
   return 0;
 }
