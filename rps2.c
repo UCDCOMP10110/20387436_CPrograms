c307bf55953a4e515141e7ba2a64f1bfbc758e76
Eoin McMahon <eoin.mcmahon1@ucdconnect.ie>
2020-10-28 12:00:52 +0000

    final rock paper scissors

---
+++ rps2.c
@@ -0,0 +1,248 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+
+  int main(void)
+  {
+  int i;
+  int randNum;
+  int seed;
+  int num;
+  int u;
+  int c;
+  u = 0;
+  c = 0;
+
+  printf("Please enter the random number generator seed.\n");
+
+  scanf("%d", &seed);
+  srand(seed);
+  printf("Please choose 1 for ROCK, 2 for PAPER or 3 for SCISSORS\n");
+
+  for (i=0;i<10;i++)
+  {
+
+  scanf("%d", &num);
+
+  randNum = rand()%3+1;
+
+
+  if (num == 1)
+  {
+    printf("User choice is ROCK.\n");
+    if (randNum == 1)
+    {
+      printf("Computer choice is ROCK.\n");
+    }
+    else if (randNum == 2)
+    {
+      printf("Computer choice is PAPER.\n");
+    }
+    else if (randNum == 3)
+    {
+      printf("Computer choice is SCISSORS.\n");
+    }
+
+    if (randNum == num)
+    {
+      printf("It's a draw. ");
+      if (randNum == 1)
+      {
+        printf("Computer chose ROCK and User also chose ROCK.\n");
+      }
+      else if (randNum == 2)
+      {
+        printf("Computer chose PAPER and User also chose PAPER.\n");
+      }
+      else
+      {
+        printf("Computer chose SCISSORS and User also chose PAPER.\n");
+      }
+    }
+    if (randNum == 1 && num == 3)
+    {
+      c = c + 1;
+      printf("Computer wins because ROCK beats SCISSORS.\n");
+    }
+    else if(randNum == 2 && num == 1)
+    {
+      c = c + 1;
+      printf("Computer wins because PAPER beats ROCK.\n");
+    }
+    else if(randNum == 3 && num == 2)
+    {
+      c = c + 1;
+      printf("Computer wins because SCISSORS beats PAPER.\n");
+    }
+    if (randNum == 1 && num == 2)
+    {
+      u = u + 1 ;
+      printf("User wins because PAPER beats ROCK.\n");
+    }
+    else if(randNum == 2 && num == 3)
+    {
+     u = u + 1;
+     printf("User wins because SCISSORS bests PAPER.\n");
+    }
+    else if(randNum == 3 && num == 1)
+    {
+      u = u + 1;
+      printf("User wins because PAPER beats ROCK.\n");
+    }
+
+  }
+  else if(num == 2)
+  {
+    printf("User choice is PAPER.\n");
+    if (randNum == 1)
+    {
+      printf("Computer choice is ROCK.\n");
+    }
+    else if (randNum == 2)
+    {
+      printf("Computer choice is PAPER.\n");
+    }
+    else if (randNum == 3)
+    {
+      printf("Computer choice is SCISSORS.\n");
+    }
+
+    if (randNum == num)
+    {
+      printf("It's a draw. ");
+      if (randNum == 1)
+      {
+        printf("Computer chose ROCK and User also chose ROCK.\n");
+      }
+      else if (randNum == 2)
+      {
+        printf("Computer chose PAPER and User also chose PAPER.\n");
+      }
+      else
+      {
+        printf("Computer chose SCISSORS and User also chose PAPER.\n");
+      }
+    }
+    if (randNum == 1 && num == 3)
+    {
+      c = c + 1;
+      printf("Computer wins because ROCK beats SCISSORS.\n");
+    }
+    else if(randNum == 2 && num == 1)
+    {
+      c = c + 1;
+      printf("Computer wins because PAPER beats ROCK.\n");
+    }
+    else if(randNum == 3 && num == 2)
+    {
+      c = c + 1;
+      printf("Computer wins because SCISSORS beats PAPER.\n");
+    }
+    if (randNum == 1 && num == 2)
+    {
+      u = u + 1 ;
+      printf("User wins because PAPER beats ROCK.\n");
+    }
+    else if(randNum == 2 && num == 3)
+    {
+     u = u + 1;
+     printf("User wins because SCISSORS bests PAPER.\n");
+    }
+    else if(randNum == 3 && num == 1)
+    {
+      u = u + 1;
+      printf("User wins because PAPER beats ROCK.\n");
+    }
+
+  }
+  else if (num == 3)
+  {
+    printf("User choice is SCISSORS\n");
+    if (randNum == 1)
+    {
+      printf("Computer choice is ROCK.\n");
+    }
+    else if (randNum == 2)
+    {
+      printf("Computer choice is PAPER.\n");
+    }
+    else if (randNum == 3)
+    {
+      printf("Computer choice is SCISSORS.\n");
+    }
+
+    if (randNum == num)
+    {
+      printf("It's a draw. ");
+      if (randNum == 1)
+      {
+        printf("Computer chose ROCK and User also chose ROCK.\n");
+      }
+      else if (randNum == 2)
+      {
+        printf("Computer chose PAPER and User also chose PAPER.\n");
+      }
+      else
+      {
+        printf("Computer chose SCISSORS and User also chose PAPER.\n");
+      }
+    }
+    if (randNum == 1 && num == 3)
+    {
+      c = c + 1;
+      printf("Computer wins because ROCK beats SCISSORS.\n");
+    }
+    else if(randNum == 2 && num == 1)
+    {
+      c = c + 1;
+      printf("Computer wins because PAPER beats ROCK.\n");
+    }
+    else if(randNum == 3 && num == 2)
+    {
+      c = c + 1;
+      printf("Computer wins because SCISSORS beats PAPER.\n");
+    }
+    if (randNum == 1 && num == 2)
+    {
+      u = u + 1 ;
+      printf("User wins because PAPER beats ROCK.\n");
+    }
+    else if(randNum == 2 && num == 3)
+    {
+     u = u + 1;
+     printf("User wins because SCISSORS bests PAPER.\n");
+    }
+    else if(randNum == 3 && num == 1)
+    {
+      u = u + 1;
+      printf("User wins because ROCK beats SCISSORS.\n");
+    }
+
+  }
+
+  else
+  {
+  i = i - 1;
+  }
+
+  }
+  printf("In 10 games, computer won %d ", c);
+  if (c == 1)
+  {
+    printf("time ");
+  }
+  else
+  {
+    printf("times ");
+  }
+  printf("and user won %d ", u);
+  if (u == 1)
+  {
+    printf("time.\n");
+  }
+  else
+  {
+    printf("times.\n");
+  }
+
+    return 0;
+  }
